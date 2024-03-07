5f1171087bd22906ea18a13c61056f44ffa8ab07
LorisRosano <l.rosano.2306@vallauri.edu>
2024-03-07 21:33:37 +0100

    Aggiunta gestione Login

--- rilievieperizie/src/app/components/login/login.component.ts
+++ rilievieperizie/src/app/components/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ServerService } from '../../servizi/server.service';
 
 @Component({
   selector: 'LoginComponent',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private server: ServerService) {}
+
+  username:string = "";
+  password:string = "";
+
+  rosso:boolean = false;
+
+  async btnLogin(){
+    let rq = await this.server.inviaRichiesta("get", "/login" , {username: this.username, password: this.password});
+    console.log(rq);
+    if(rq)
+      alert("DIO PORCO! Sei loggato!");
+    else
+      this.rosso = true;
+     
+  }
 }
---
+++ rilievieperizie/src/app/servizi/server.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  private REST_API_SERVER = 'http://localhost:3000/api';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public inviaRichiesta(method: string, resource: string, params: any = {} ): Promise<any> | undefined {
+    resource = this.REST_API_SERVER + resource;
+    switch (method.toLowerCase()) {
+      case 'get':
+        return firstValueFrom(this.httpClient.get(resource, { params: params }));
+      case 'delete':
+        return  firstValueFrom(this.httpClient.delete(resource, { body: params }));
+      case 'post':
+        return  firstValueFrom(this.httpClient.post(resource, params));
+      case 'patch':
+        return  firstValueFrom(this.httpClient.patch(resource, params));
+      case 'put':
+        return  firstValueFrom(this.httpClient.put(resource, params));
+      default:
+        return undefined;
+    }
+  }
+}
