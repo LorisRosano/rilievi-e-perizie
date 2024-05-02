66fc67882e608297b683cc3e42866c88eca1a0e7
LorisRosano <l.rosano.2306@vallauri.edu>
2024-05-02 18:56:18 +0200

    CORS Policy 2

--- rilievieperizie/Crud-Server/server.ts
+++ rilievieperizie/Crud-Server/server.ts
@@ -87,6 +87,7 @@ const corsOptions = {
     credentials: true
 };
 app.use("/", _cors(corsOptions));
+app.use(_cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
